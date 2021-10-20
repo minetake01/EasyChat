81538074ec66a579ac3afbef5b04664e1c60c8cd
minetake <minetake01.m@gmail.com>
2021-10-20 23:56:33 +0900

    master

---
+++ serviceWorker/lib.js
@@ -0,0 +1,11 @@
+//配信の詳細を取得するURLリスト
+const urls = [
+	'https://www.youtube.com/watch*',
+	'https://live.nicovideo.jp/watch*',
+	'https://www.twitch.tv/*',
+	'https://www.showroom-live.com/*',
+	'https://www.openrec.tv/live*',
+	'https://www.mildom.com/*',
+	'https://twitcasting.tv/*',
+	'https://0000.studio/*'
+];
--- serviceWorker/service_worker.js
+++ serviceWorker/service_worker.js
@@ -1,15 +1,5 @@
 importScripts('./lib.js');
 
-const urls = [
-	'https://www.youtube.com/watch*',
-	'https://live.nicovideo.jp/watch*',
-	'https://www.twitch.tv/*',
-	'https://www.showroom-live.com/*',
-	'https://www.openrec.tv/live*',
-	'https://www.mildom.com/*',
-	'https://twitcasting.tv/*',
-	'https://0000.studio/*'
-];
 //チャンネル選択ウィンドウの初期状態
 const windowOption = {
 	focused: true,
