f8eeb744946c3d7eed2d8632e93bf78a22a498b4
minetake <minetake01.m@gmail.com>
2021-11-16 00:06:50 +0900

    master

--- streamDetail_getter/common/niconico.js
+++ streamDetail_getter/common/niconico.js
@@ -1,4 +1,7 @@
-let platform = 'nicovideo'
-let chatOK = !!$('#root > div > div.___player-area___1IipP > div.___player-body-area___3W2NR > div > div > div.___player-display___1vZk7 > div.___player-display-footer___-aMrY > div > div.___comment-post-panel___153p6.___ga-ns-comment-post-panel___2Fh8_.___comment-post-panel___DHJYr > form > input[style!="user-select: none;"]').length;
+let platform = 'niconico'
+let chatOK = !$('.___overlay___3Nw78').length;
 let streamURL = location.href;
-let streamTitle = $('#root > div > div.___program-information-area___2Wcf4 > div.___program-information-header-area___2AbSa > div > div.___primary-information-area___16K0E > h1 > span').html();
+let streamTitle = $('.___program-title___3pozg>span').html();
+let channelName = $('.___user-name___I7dL6>[class=name]').html();
+let channelID = $('.___user-name___I7dL6').attr('href');
+let other
