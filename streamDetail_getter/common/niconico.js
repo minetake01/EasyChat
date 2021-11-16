27e13f1eaac0e34c8c378f5334b7df21f5c78214
minetake <minetake01.m@gmail.com>
2021-11-16 23:15:30 +0900

    master

--- streamDetail_getter/common/niconico.js
+++ streamDetail_getter/common/niconico.js
@@ -2,6 +2,13 @@ let platform = 'niconico'
 let chatOK = !$('.___overlay___3Nw78').length;
 let streamURL = location.href;
 let streamTitle = $('.___program-title___3pozg>span').html();
-let channelName = $('.___user-name___I7dL6>[class=name]').html();
-let channelID = $('.___user-name___I7dL6').attr('href');
+let channelName;
+let channelID;
+if ($('.___program-provider-type-label___3VgIC').length) {
+	channelName = $('div.___channel-information-area___1kBok>a').html();
+	channelID = $('div.___channel-information-area___1kBok>a').attr('href');
+} else {
+	channelName = $('.___user-name___DbpE9>[class=name]').html();
+	channelID = $('.___user-name___DbpE9').attr('href');
+};
 let other
