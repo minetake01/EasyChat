42f637ba9c8adb824971bd45d40cd42eb5b5dfab
minetake <minetake01.m@gmail.com>
2021-11-21 22:01:35 +0900

    master

--- streamDetail_getter/common/twitch.js
+++ streamDetail_getter/common/twitch.js
@@ -1,4 +1,19 @@
 let platform = 'twitch'
-let chatOK = !!$('textarea').length;
+let chatOK;
 let streamURL = location.href;
-let streamTitle = $('[data-a-target="stream-title"]').html();
+let streamTitle;
+let channelName;
+let channelID;
+let other;
+
+const target = document.getElementsByClassName('logged-in')[0];
+const observer = new MutationObserver(() => {
+	if (!!$('.chat-input__textarea textarea').length) {
+		chatOK = !!$('.chat-input__textarea textarea').length;
+		streamTitle = $('h2[data-a-target="stream-title"]').html();
+		channelName = $('[href="' + location.pathname + '"] > h1').html();
+		channelID = location.href;
+		other
+	};
+});
+observer.observe(target, {childList: true});
